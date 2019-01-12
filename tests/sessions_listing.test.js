2d9cb044f79f32d000b660c14c46c280e1e3fdaa
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-12 16:09:45 +0000

    set up test for sessions stopping to look into app save state

---
+++ tests/sessions_listing.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SessionListings from '../src/components/SessionListings.js'
+
+describe( 'SessionListings', () => {
+  function testID(id) {
+    return cmp => cmp.props().testID === id;
+  }
+  it('Displays the lists of sessions', () => {
+
+  });
+});
